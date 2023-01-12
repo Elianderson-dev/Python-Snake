cb51718520fd7ec04f476dfdc5ae2a9faffc1c8d
elianderson-dev <nin2333124568@gmail.com>
2023-01-12 21:21:11 +0000

    feat: adds and moves the snake

---
+++ game.py
@@ -0,0 +1,37 @@
+import pygame as pg
+from random import randrange
+
+WINDOW = 500
+TILE_SIZE = 50
+RANGE = (TILE_SIZE // 2, WINDOW - TILE_SIZE // 2, TILE_SIZE)
+get_random_position = lambda: [randrange(*RANGE), randrange(*RANGE)]
+snake = pg.rect.Rect([0, 0, TILE_SIZE - 2, TILE_SIZE - 2])
+snake.center = get_random_position()
+lenght = 1
+segments = [snake.copy()]
+snake_dir = (0, 0)
+screen = pg.display.set_mode([WINDOW] * 2)
+clock = pg.time.Clock()
+
+while True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            exit()
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_w:
+                snake_dir = (0, -TILE_SIZE)
+            if event.key == pg.K_s:
+                snake_dir = (0, TILE_SIZE)
+            if event.key == pg.K_a:
+                snake_dir = (-TILE_SIZE, 0)
+            if event.key == pg.K_d:
+                snake_dir = (TILE_SIZE, 0)
+            screen.fill("black")
+            # add the snake on the screen
+            [pg.draw.rect(screen, 'green', segment) for segment in segments]
+            # move the snake
+            snake.move_ip(snake_dir)
+            segments.append(snake,copy())
+            semnents = segments[-length:]
+            pg.display.flip()
+            clock.tick(60)
