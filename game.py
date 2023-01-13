9407ac98f08d0e7ef22f3e602dc4c20d9e980612
elianderson-dev <nin2333124568@gmail.com>
2023-01-13 08:32:27 +0000

    feat: adds self collision

--- game.py
+++ game.py
@@ -1,10 +1,10 @@
 import pygame as pg
 from random import randrange
 
-WINDOW = 1000
+WINDOW = 600
 TILE_SIZE = 50
 RANGE = (TILE_SIZE // 2, WINDOW - TILE_SIZE // 2, TILE_SIZE)
-get_random_position = lambda: [randrange(*RANGE), randrange(*RANGE)]
+get_random_position = Lambda: [randrange(*RANGE), randrange(*RANGE)]
 snake = pg.rect.Rect([0, 0, TILE_SIZE - 2, TILE_SIZE - 2])
 snake.center = get_random_position()
 length = 1
@@ -15,22 +15,26 @@ food = snake.copy()
 food.center = get_random_position()
 screen = pg.display.set_mode([WINDOW] * 2)
 clock = pg.time.Clock()
-
+dirs = {pg.K_w: 1, pg.K_s: 1, pg.K_a: 1, pg.K_d: 1}
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             exit()
         if event.type == pg.KEYDOWN:
-            if event.key == pg.K_w:
+            if event.key == pg.K_w and dirs[pg.K_w]:
                 snake_dir = (0, -TILE_SIZE)
-            if event.key == pg.K_s:
+                dirs = {pg.K_w: 1, pg.K_s: 0, pg.K_a: 1, pg.K_d: 1}
+            if event.key == pg.K_s and dirs[pg.K_s]:
                 snake_dir = (0, TILE_SIZE)
-            if event.key == pg.K_a:
+                dirs = {pg.K_w: 0, pg.K_s: 1, pg.K_a: 1, pg.K_d: 1}
+            if event.key == pg.K_a and dirs[pg.K_a]:
                 snake_dir = (-TILE_SIZE, 0)
-            if event.key == pg.K_d:
+                dirs = {pg.K_w: 1, pg.K_s: 1, pg.K_a: 1, pg.K_d: 0}
+            if event.key == pg.K_d and dirs[pg.K_d]:
                 snake_dir = (TILE_SIZE, 0)
+                dirs = {pg.K_w: 1, pg.K_s: 1, pg.K_a: 0, pg.K_d: 1}
             screen.fill("black")
-            # adds border impact
+            # adds border impact and self collision
             if snake.left < 0 or snake.right > WINDOW or snake.top < 0 or snake.bottom > WINDOW:
                 snake.center, food.center = get_random_position(), get_random_position()
                 length, snake_dir = 1, (0, 0)
